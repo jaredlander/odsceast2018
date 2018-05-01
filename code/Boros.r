ef10a42621be92308f3052665b52ec8932155c7e
Jared Lander <info@jaredlander.com>
2018-05-01 09:19:10 -0400

    Adding boros data

---
+++ code/Boros.r
@@ -0,0 +1,8 @@
+boros <- tibble::tribble(
+    ~ Boro, ~ Pop, ~ Size, ~ Random,
+    'Manhattan', 1600000, 23, 7,
+    'Brooklyn', 2600000, 78, 24,
+    'Queens', 2330000, 104, pi,
+    'Bronx', 1455000, 42, 21,
+    'Staten Island', 475000, 60, 3
+)
